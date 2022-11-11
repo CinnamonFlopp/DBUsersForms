6a04ba92bfc7c9df231828606cdb692d4508a4e1
CinnamonFlopp <102373575+CinnamonFlopp@users.noreply.github.com>
2022-11-11 10:11:30 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Citilink;
+
+namespace Citilink
+{
+    public partial class Main : Form
+    {
+        DataBase DataBase = new DataBase();
+        public Main()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            textBox_Password.PasswordChar = '*';
+            textBox_Login.MaxLength = 50;
+            textBox_Password.MaxLength = 50;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_Login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var loginUser = textBox_Login.Text;
+            var password = textBox_Password.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"select id,login,password from register where login = '{loginUser}' and password = '{password}'";
+            SqlCommand command = new SqlCommand(querystring, DataBase.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count==1)
+            {
+                MessageBox.Show("Вы успешно вошли!","Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DataGrid dtg = new DataGrid();
+                dtg.Show();
+            }
+            else
+            {
+                MessageBox.Show("Такого пользователя не существует!", "Попробуйте ещё раз!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
